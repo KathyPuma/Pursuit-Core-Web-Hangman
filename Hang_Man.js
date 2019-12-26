0a767691d4704847462f61ac2066f60ade2552d3
KathyPuma <kathypuma96@gmail.com>
2019-12-26 18:40:53 -0500

    limit on how many tries works. Conditions to input response added.

--- Hang_Man.js
+++ Hang_Man.js
@@ -1,6 +1,6 @@
-// User must play against computer 
-// words that the computer chooses must be random.
-// users have a  limited number of guesses
+// User must play against computer --Complete
+// words that the computer chooses must be random. --Complete
+// users have a  limited number of guesses -Complete
 // After each guess, the computer should print out the current state of the word you are guessing. (ex. A _ _ L E, for apple)
 // After each guess the computer should print out how many guesses you have remaining.
 // The computer should tell the user if she wins, show her how many guesses it took and end the current game
@@ -19,7 +19,6 @@ const getRandomWord = (arr) => {
 
 const setupHiddenWord = (randomWord) => {
   let hiddenWord = []
-  console.log(randomWord.length)
   for (let i = 0; i < randomWord.length; i++) {
     hiddenWord.push("_")
   }
@@ -30,32 +29,67 @@ const checkIfLetterExist = (letter, randomWord) => {
   return randomWord.includes(letter)
 }
 
+const replaceWithLetter = (randomWord, letter, hiddenWord) => {
+  for (let i = 0; i < randomWord.length; i++) {
+    if (randomWord[i] === letter) {
+      hiddenWord[i] = letter
+    }
+  }
+  return hiddenWord
+}
+
+
+const winner = (tries, hiddenWord, randomWord) => {
+  if (tries === 0) {
+    return "You Lose!"
+  } else if (hiddenWord === randomWord) {
+    return "You Win!"
+
+  }
+}
 
 
 
 const beginGame = () => {
   let generatedRandomWord = getRandomWord(words)
   console.log(generatedRandomWord)
-  let blank = setupHiddenWord(generatedRandomWord)
-  console.log(blank)
-  const letterInput = readlineSync.question("Type a letter: ")
-  console.log(letterInput)
+  let hiddenWord = setupHiddenWord(generatedRandomWord)
+  let hiddenWordJoin = hiddenWord.join('')
+  let allInput = []
 
   let tries = 6
+  while (tries > 0 && !(hiddenWordJoin === generatedRandomWord)) {
+    const letterInput = readlineSync.question("Type a letter: ")
+    // let allInputJoin = allInput.join("")
 
-  if (checkIfLetterExist(letterInput, generatedRandomWord)) {
-    console.log("letter exists")
+    if (letterInput.length > 1) {
+      console.log("only 1 letter")
+
+    }else if(letterInput.length === 0){
+      console.log("No blank input")
+
+    } else {
+      allInput.push(letterInput)
+      console.log(`All of your guesses: ${allInput}`)
+      if (checkIfLetterExist(letterInput, generatedRandomWord)) {
+        console.log("letter exists")
+        hiddenWord = replaceWithLetter(generatedRandomWord, letterInput, hiddenWord)
+        console.log(hiddenWord)
+      } else {
+        console.log(`Wrong! You have ${tries - 1} tries left`)
+        tries--
+        console.log(tries)
+      }
+    }
 
 
-  } else {
-    console.log("wrong")
-    tries--
-    console.log(tries)
   }
 
 
 
-
-
+  let endOfGame = winner(tries, hiddenWordJoin, generatedRandomWord)
+  console.log(endOfGame)
 }
+
+
 beginGame()
