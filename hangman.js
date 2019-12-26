53a15fbaca9c8e0cc36debb40fe500e74a533496
KathyPuma <kathypuma96@gmail.com>
2019-12-26 13:54:12 -0500

    setup and created random word function

--- hangman.js
+++ hangman.js
@@ -1,17 +1,27 @@
-const readline = require('readline-sync');
+// User must play against computer 
+// words that the computer chooses must be random.
+// users have a  limited number of guesses
+// After each guess, the computer should print out the current state of the word you are guessing. (ex. A _ _ L E, for apple)
+// After each guess the computer should print out how many guesses you have remaining.
+// The computer should tell the user if she wins, show her how many guesses it took and end the current game
+// The user should see the correct answer if she loses
 
-function getValidLetterGuess() {
-  function guessIsValid(letter) {
-    return letter.length === 1 && letter.toUpperCase() != letter.toLowerCase()
-  }
-  let letter = ""
-  while (!letter) {
-    let input = readline.question("Please enter your guess: ")
-    if (guessIsValid(input)) {
-      letter = input
-    } else {
-      console.log("Please enter a valid letter")
-    }
-  }
-  return letter.toLowerCase()
+
+var readlineSync = require('readline-sync');
+
+
+let words= ["hello", "happy"]
+
+
+const getRandomWord = (arr) =>{
+  let random = arr[Math.floor(Math.random()*arr.length)]
+  return random 
 }
+
+
+const beginGame = () =>{
+  let generatedWord = getRandomWord(words)
+  console.log(generatedWord)
+}
+
+beginGame()
